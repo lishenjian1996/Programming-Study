f0a463df0bd9232ef29e5479d34a2355d7c72cfe
Shenjian Li <lishenjian2013@live.com>
2023-10-24 00:24:43 +0800

    Newly added

---
+++ Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/friendly.py
@@ -0,0 +1,11 @@
+# A module that I am goign to use as an example
+
+def greet(what='Hello There', to='Mystry Person'):
+    """
+    A convenience function that returns a friendly string
+    Parameters:
+        what (str): The greeting to issue
+        to (str): The person to greet
+    Returns:
+        a string that greets the person"""
+    return f'{what} {to} (new version!)'
