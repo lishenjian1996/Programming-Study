6e10abaea98a1bd1cbb4393b26d6180a064a719c
Shenjian Li <lishenjian2013@live.com>
2023-10-29 23:35:34 +0800

    modified:   Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/__pycache__/edhc_risk_kit.cpython-311.pyc 	modified:   Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/__pycache__/edhec_risk_kit.cpython-311.pyc 	modified:   Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/edhec_risk_kit.py 	modified:   Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/week2-implementing markowitz.ipynb 	new file:   Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/week3-beyond diversification-1.ipynb

--- Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/edhec_risk_kit.py
+++ Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/week1/edhec_risk_kit.py
@@ -247,7 +247,40 @@ def optimal_weights(n_points,er,cov):
     weights=[minimize_vol(target_return, er, cov) for target_return in target_rs]
     return weights
 
-def plot_ef(n_points,er,cov, style='.-'):
+def msr(riskfree_rate, er, cov):
+    """
+    Returns the weights of the portfolio that gives you the maximum sharpe ratio
+    Given the riskfree rate and expected returns and a covariance matrix.
+    """
+    n=er.shape[0]
+    init_guess=np.repeat(1/n, n)
+    bounds=((0.0,1.0), )*n
+    weights_sum_to_1={
+        'type': 'eq',
+        'fun': lambda weights: np.sum(weights)-1
+    }
+    def neg_sharpe_ratio(weights,riskfree_rate, er, cov):
+        """
+        Returns the negative of the sharpe ratio, given weights"""
+        r= portfolio_return(weights, er)
+        vol=portfolio_vol(weights, cov)
+        return -(r-riskfree_rate)/vol
+    
+    results=minimize(neg_sharpe_ratio, init_guess,
+                     args=(riskfree_rate, er, cov), method='SLSQP',
+                     options={'disp':False},
+                     constraints=(weights_sum_to_1),
+                     bounds=bounds)
+    return results.x
+def gmv(cov):
+    """
+    Return the wight of the Global minimum vol portfolio
+    given the covariance matrix
+    """
+    n=cov.shape[0]
+    return msr(0, np.repeat(1,n),cov)
+
+def plot_ef(n_points,er,cov, show_cml=False, style='.-', riskfree_rate=0, show_ew=False, show_gmv=False):
     """
     Plots the muliti-asset efficient frontier
     """
@@ -258,4 +291,30 @@ def plot_ef(n_points,er,cov, style='.-'):
         "Returns":rets,
         "Volatility":vols
     })
-    return ef.plot.line(x='Volatility', y='Returns', style=style)
+    ax= ef.plot.line(x='Volatility', y='Returns', style=style)
+    if show_ew:
+        n=er.shape[0]
+        w_ew=np.repeat(1/n,n)
+        r_ew=portfolio_return(w_ew, er)
+        vol_ew=portfolio_vol(w_ew, cov)
+        # display Equally weight portfolio
+        ax.point([vol_ew],[r_ew], color='goldenrod', marker='o', markersize=10)
+
+    if show_gmv:
+        w_gmv=gmv(cov)
+        r_gmv=portfolio_return(w_gmv, er)
+        vol_gmv=portfolio_vol(w_gmv, cov)
+        # dis GMV
+        ax.point([vol_gmv],[r_gmv], color='midnightblue', marker='o', markersize=10)
+
+    if show_cml:
+        ax.set_xlim(left=0)
+        w_msr=msr(riskfree_rate,er,cov)
+        r_msr=portfolio_return(w_msr,er)
+        vol_msr=portfolio_vol(w_msr,cov)
+        # Add CML
+        cml_x=[0, vol_msr]
+        cml_y=[riskfree_rate, r_msr]
+        ax.plot(cml_x,cml_y, color='cyan', marker='o', linestyle='dashed', markersize=12, linewidth=2)
+
+    return ax
