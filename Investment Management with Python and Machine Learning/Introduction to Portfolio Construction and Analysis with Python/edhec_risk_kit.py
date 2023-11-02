4d585953b75bde9602872711e5361188a050eecd
Shenjian Li <lishenjian2013@live.com>
2023-11-02 23:16:50 +0800

    updated

--- Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/edhec_risk_kit.py
+++ Investment Management with Python and Machine Learning/Introduction to Portfolio Construction and Analysis with Python/edhec_risk_kit.py
@@ -452,3 +452,130 @@ def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, 
     rets_plus_1[0] = 1
     ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
     return ret_val
+
+
+def funding_ratio(assets, liabilities, r):
+    """
+    Computes the findina ratio of some assets given liabilities and interest rate
+    """
+    return assets/pv(liabilities, r)
+
+
+def pv(l,r):
+    """
+    Computes the present value of a sequence of liabilities
+    l is indexed by the time, and the values are the amounts of each liability
+    returns the present value of the sequence
+    """
+    dates= l.index
+    discounts=discount(dates,r)
+    return (discounts*l).sum()
+
+
+def discount(t,r):
+    """
+    Compute the price of a pure discount bond that pays a dollar at time t, given interest rate r
+    """
+    return 1/(1+r)**t #or (1+t)**(-t)
+
+
+import numpy as np
+import pandas as pd
+import edhec_risk_kit as erk
+%load_ext autoreload
+%autoreload 2
+
+def inst_to_ann(r):
+    """
+    Convert an short rate (instantaneous interest rate) to an annual interest rate
+    """
+    return np.expm1(r) # equally with np.exp(r)-1
+
+def ann_to_inst(r):
+    """
+    Convert an annual interest rate to an short rate (instantaneous interest rate)  
+    """
+    return np.log1p(r) # equally with np.log(1+r)
+
+def cir(n_years = 10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Implements the CIR model for interest rates
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None: r_0 = b 
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 # because n_years might be a float
+    
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios)) #generate random numbers
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step] # check the below formula
+        rates[step] = abs(r_t + d_r_t) # just in case of roundoff errors going negative
+        
+    return pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+
+%matplotlib inline
+
+
+import math
+def cir(n_years = 10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None: r_0 = b 
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 # because n_years might be a float
+    
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    ## For Price Generation
+    h = math.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    ####
+
+    def price(ttm, r):
+        _A = ((2*h*math.exp((h+a)*ttm/2))/(2*h+(h+a)*(math.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(math.exp(h*ttm)-1))/(2*h + (h+a)*(math.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    ####
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years-step*dt, rates[step])
+
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
+
+def show_cir_prices(r_0=0.03, a=0.5, b=0.03, sigma=0.05, n_scenarios=5):
+    cir(r_0=r_0, a=a, b=b, sigma=sigma, n_scenarios=n_scenarios)[1].plot(legend=False, figsize=(12,5))
+
+controls = widgets.interactive(show_cir_prices,
+                              r_0 = (0, .15, .01),
+                              a = (0, 1, .1),
+                               b = (0, .15, .01),
+                               sigma= (0, .1, .01),
+                               n_scenarios = (1, 100))
+display(controls)
+
+# as interest rate go up, the price of bond down
+# as interest rate go down, the price of bond up
+
+
