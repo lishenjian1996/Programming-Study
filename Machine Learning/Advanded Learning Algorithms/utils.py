786209e01134603f091b1558c488695f91662a9e
Shenjian Li <lishenjian2013@live.com>
2023-11-11 00:15:39 +0800

    updates

--- Machine Learning/Advanded Learning Algorithms/utils.py
+++ Machine Learning/Advanded Learning Algorithms/utils.py
@@ -1,44 +1,373 @@
-# C2_W1 Utilities
 import numpy as np
+
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+import matplotlib.lines as mlines
+plt.style.use('./deeplearning.mplstyle')
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-# Plot  multi-class training points
-def plot_mc_data(X, y, class_labels=None, legend=False,size=40):
-    classes = np.unique(y)
-    for i in classes:
-        label = class_labels[i] if class_labels else "class {}".format(i)
-        idx = np.where(y == i)
-        plt.scatter(X[idx, 0], X[idx, 1],  cmap=plt.cm.Paired,
-                    edgecolor='black', s=size, label=label)
-    if legend: plt.legend()
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+def plot_dataset(x, y, title):
+    plt.rcParams["figure.figsize"] = (12,8)
+    plt.rcParams["lines.markersize"] = 12
+    plt.scatter(x, y, marker='x', c='r'); 
+    plt.title(title)
+    plt.xlabel("x"); 
+    plt.ylabel("y"); 
+    plt.show()
+
+def plot_train_cv_test(x_train, y_train, x_cv, y_cv, x_test, y_test, title):
+    plt.scatter(x_train, y_train, marker='x', c='r', label='training'); 
+    plt.scatter(x_cv, y_cv, marker='o', c='b', label='cross validation'); 
+    plt.scatter(x_test, y_test, marker='^', c='g', label='test'); 
+    plt.title("input vs. target")
+    plt.xlabel("x"); 
+    plt.ylabel("y"); 
+    plt.legend()
+    plt.show()
+
+def plot_train_cv_mses(degrees, train_mses, cv_mses, title):
+    degrees = range(1,11)
+    plt.plot(degrees, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(degrees, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.title(title)
+    plt.xlabel("degree"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+def plot_bc_dataset(x, y, title):
+    for i in range(len(y)):
+        marker = 'x' if y[i] == 1 else 'o'
+        c = 'r' if y[i] == 1 else 'b'
+        plt.scatter(x[i,0], x[i,1], marker=marker, c=c); 
+    plt.title("x1 vs x2")
+    plt.xlabel("x1"); 
+    plt.ylabel("x2"); 
+    y_0 = mlines.Line2D([], [], color='r', marker='x', markersize=12, linestyle='None', label='y=1')
+    y_1 = mlines.Line2D([], [], color='b', marker='o', markersize=12, linestyle='None', label='y=0')
+    plt.title(title)
+    plt.legend(handles=[y_0, y_1])
+    plt.show()
+
+def build_models():
+    
+    tf.random.set_seed(20)
+    
+    model_1 = Sequential(
+        [
+            Dense(25, activation = 'relu'),
+            Dense(15, activation = 'relu'),
+            Dense(1, activation = 'linear')
+        ],
+        name='model_1'
+    )
+
+    model_2 = Sequential(
+        [
+            Dense(20, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(20, activation = 'relu'),
+            Dense(1, activation = 'linear')
+        ],
+        name='model_2'
+    )
+
+    model_3 = Sequential(
+        [
+            Dense(32, activation = 'relu'),
+            Dense(16, activation = 'relu'),
+            Dense(8, activation = 'relu'),
+            Dense(4, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(1, activation = 'linear')
+        ],
+        name='model_3'
+    )
+    
+    model_list = [model_1, model_2, model_3]
+    
+    return model_list
+
+
+# Not used in the lab. You can call this for the binary 
+# classification problem if you set `from_logits=False` 
+# when declaring the loss. With this, you will also not need
+# to call the `tf.math.sigmoid()` function in the loop
+# because the model output is already a probability
+def build_bc_models():
+
+    tf.random.set_seed(20)
+
+    model_1_bc = Sequential(
+        [
+            Dense(25, activation = 'relu'),
+            Dense(15, activation = 'relu'),
+            Dense(1, activation = 'sigmoid')
+        ],
+        name='model_1_bc'
+    )
+
+    model_2_bc = Sequential(
+        [
+            Dense(20, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(20, activation = 'relu'),
+            Dense(1, activation = 'sigmoid')
+        ],
+        name='model_2_bc'
+    )
+
+    model_3_bc = Sequential(
+        [
+            Dense(32, activation = 'relu'),
+            Dense(16, activation = 'relu'),
+            Dense(8, activation = 'relu'),
+            Dense(4, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(1, activation = 'sigmoid')
+        ],
+        name='model_3_bc'
+    )
+
+    models_bc = [model_1_bc, model_2_bc, model_3_bc]
+    
+    return models_bc
+
+
+def prepare_dataset(filename):
+    
+    data = np.loadtxt(filename, delimiter=",")
+    
+    x = data[:,:-1]
+    y = data[:,-1]
+    
+    # Get 60% of the dataset as the training set. Put the remaining 40% in temporary variables.
+    x_train, x_, y_train, y_ = train_test_split(x, y, test_size=0.40, random_state=80)
+
+    # Split the 40% subset above into two: one half for cross validation and the other for the test set
+    x_cv, x_test, y_cv, y_test = train_test_split(x_, y_, test_size=0.50, random_state=80)
+    
+    return x_train, y_train, x_cv, y_cv, x_test, y_test
+
+def train_plot_poly(model, x_train, y_train, x_cv, y_cv, max_degree=10, baseline=None):
+    
+    train_mses = []
+    cv_mses = []
+    models = []
+    scalers = []
+    degrees = range(1,max_degree+1)
+
+    # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+    for degree in degrees:
+
+        # Add polynomial features to the training set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_train_mapped = poly.fit_transform(x_train)
+
+        # Scale the training set
+        scaler_poly = StandardScaler()
+        X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+        scalers.append(scaler_poly)
+
+        # Create and train the model
+        model.fit(X_train_mapped_scaled, y_train )
+        models.append(model)
+
+        # Compute the training MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        train_mse = mean_squared_error(y_train, yhat) / 2
+        train_mses.append(train_mse)
+
+        # Add polynomial features and scale the cross-validation set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_cv_mapped = poly.fit_transform(x_cv)
+        X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+        # Compute the cross-validation MSE
+        yhat = model.predict(X_cv_mapped_scaled)
+        cv_mse = mean_squared_error(y_cv, yhat) / 2
+        cv_mses.append(cv_mse)
+
+    # Plot the results
+    plt.plot(degrees, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(degrees, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.plot(degrees, np.repeat(baseline, len(degrees)), linestyle='--', label='baseline')
+    plt.title("degree of polynomial vs. train and CV MSEs")
+    plt.xticks(degrees)
+    plt.xlabel("degree"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+    
+def train_plot_reg_params(reg_params, x_train, y_train, x_cv, y_cv, degree= 1, baseline=None):
+    
+    train_mses = []
+    cv_mses = []
+    models = []
+    scalers = []
+
+    # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+    for reg_param in reg_params:
+
+        # Add polynomial features to the training set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_train_mapped = poly.fit_transform(x_train)
+
+        # Scale the training set
+        scaler_poly = StandardScaler()
+        X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+        scalers.append(scaler_poly)
+
+        # Create and train the model
+        model = Ridge(alpha=reg_param)
+        model.fit(X_train_mapped_scaled, y_train)
+        models.append(model)
+
+        # Compute the training MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        train_mse = mean_squared_error(y_train, yhat) / 2
+        train_mses.append(train_mse)
+
+        # Add polynomial features and scale the cross-validation set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_cv_mapped = poly.fit_transform(x_cv)
+        X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+        # Compute the cross-validation MSE
+        yhat = model.predict(X_cv_mapped_scaled)
+        cv_mse = mean_squared_error(y_cv, yhat) / 2
+        cv_mses.append(cv_mse)
+
+    # Plot the results
+    reg_params = [str(x) for x in reg_params]
+    plt.plot(reg_params, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(reg_params, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.plot(reg_params, np.repeat(baseline, len(reg_params)), linestyle='--', label='baseline')
+    plt.title("lambda vs. train and CV MSEs")
+    plt.xlabel("lambda"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+def train_plot_diff_datasets(model, files, max_degree=10, baseline=None):
+    
+    for file in files:
         
+        x_train, y_train, x_cv, y_cv, x_test, y_test = prepare_dataset(file['filename'])
 
-#Plot a multi-class categorical decision boundary
-# This version handles a non-vector prediction (adds a for-loop over points)
-def plot_cat_decision_boundary(X,predict , class_labels=None, legend=False, vector=True):
+        train_mses = []
+        cv_mses = []
+        models = []
+        scalers = []
+        degrees = range(1,max_degree+1)
 
-    # create a mesh to points to plot
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    h = max(x_max-x_min, y_max-y_min)/200
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    points = np.c_[xx.ravel(), yy.ravel()]
+        # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+        for degree in degrees:
 
-    #make predictions for each point in mesh
-    if vector:
-        Z = predict(points)
-    else:
-        Z = np.zeros((len(points),))
-        for i in range(len(points)):
-            Z[i] = predict(points[i].reshape(1,2))
-    Z = Z.reshape(xx.shape)
+            # Add polynomial features to the training set
+            poly = PolynomialFeatures(degree, include_bias=False)
+            X_train_mapped = poly.fit_transform(x_train)
 
-    #contour plot highlights boundaries between values - classes in this case
-    plt.figure()
-    plt.contour(xx, yy, Z, colors='g') 
-    plt.axis('tight')
+            # Scale the training set
+            scaler_poly = StandardScaler()
+            X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+            scalers.append(scaler_poly)
+
+            # Create and train the model
+            model.fit(X_train_mapped_scaled, y_train )
+            models.append(model)
+
+            # Compute the training MSE
+            yhat = model.predict(X_train_mapped_scaled)
+            train_mse = mean_squared_error(y_train, yhat) / 2
+            train_mses.append(train_mse)
+
+            # Add polynomial features and scale the cross-validation set
+            poly = PolynomialFeatures(degree, include_bias=False)
+            X_cv_mapped = poly.fit_transform(x_cv)
+            X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+            # Compute the cross-validation MSE
+            yhat = model.predict(X_cv_mapped_scaled)
+            cv_mse = mean_squared_error(y_cv, yhat) / 2
+            cv_mses.append(cv_mse)
+
+        # Plot the results
+        plt.plot(degrees, train_mses, marker='o', c='r', linestyle=file['linestyle'], label=f"{file['label']} training MSEs"); 
+        plt.plot(degrees, cv_mses, marker='o', c='b', linestyle=file['linestyle'], label=f"{file['label']} CV MSEs"); 
+
+    plt.plot(degrees, np.repeat(baseline, len(degrees)), linestyle='--', label='baseline')
+    plt.title("degree of polynomial vs. train and CV MSEs")
+    plt.xticks(degrees)
+    plt.xlabel("degree"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+def train_plot_learning_curve(model, x_train, y_train, x_cv, y_cv, degree= 1, baseline=None):
+    
+    train_mses = []
+    cv_mses = []
+    models = []
+    scalers = []
+    num_samples_train_and_cv = []
+    percents = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+    # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+    for percent in percents:
+        
+        num_samples_train = round(len(x_train) * (percent/100.0))
+        num_samples_cv = round(len(x_cv) * (percent/100.0))
+        num_samples_train_and_cv.append(num_samples_train + num_samples_cv)
+        
+        x_train_sub = x_train[:num_samples_train]
+        y_train_sub = y_train[:num_samples_train]
+        x_cv_sub = x_cv[:num_samples_cv]
+        y_cv_sub = y_cv[:num_samples_cv]
+
+        # Add polynomial features to the training set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_train_mapped = poly.fit_transform(x_train_sub)
+
+        # Scale the training set
+        scaler_poly = StandardScaler()
+        X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+        scalers.append(scaler_poly)
+
+        # Create and train the model
+        model.fit(X_train_mapped_scaled, y_train_sub)
+        models.append(model)
+
+        # Compute the training MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        train_mse = mean_squared_error(y_train_sub, yhat) / 2
+        train_mses.append(train_mse)
+
+        # Add polynomial features and scale the cross-validation set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_cv_mapped = poly.fit_transform(x_cv_sub)
+        X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+        # Compute the cross-validation MSE
+        yhat = model.predict(X_cv_mapped_scaled)
+        cv_mse = mean_squared_error(y_cv_sub, yhat) / 2
+        cv_mses.append(cv_mse)
+
+    # Plot the results
+    plt.plot(num_samples_train_and_cv, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(num_samples_train_and_cv, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.plot(num_samples_train_and_cv, np.repeat(baseline, len(percents)), linestyle='--', label='baseline')
+    plt.title("number of examples vs. train and CV MSEs")
+    plt.xlabel("total number of training and cv examples"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
